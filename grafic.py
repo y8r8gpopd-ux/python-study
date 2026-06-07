13892fedf7a10e3fb85815db43422841f72fad5b
y8r8gpopd-ux <y8r8g.popd@gmail.com>
2026-06-07 21:08:36 +0900

    描画を用いたクラストインスタンスの学習02

--- grafic.py
+++ grafic.py
@@ -6,19 +6,38 @@ circle = None
 x = 400
 y = 300
 
+dx = 1
+dy = 1
+
 # クリックした場所に円の作成
 def move():
   # 関数外から読み込み
-  global circle, x, y
+  global circle, x, y, dx, dy
 
   # 描画した円が残っていたら削除
   if circle != None:
     canvas.delete(circle)
   
-  x += 1
+  # 座標を移動させるdx,dy
+  x += dx
+  y += dy
+
   # 円の描画
   circle = canvas.create_oval(x - 20, y - 20, x + 20, y + 20, fill = "red", width = 0)
-  root.after(30, move)
+
+  # 画面端にたどり着いたら向きを変える仕組み x軸
+  if x >= canvas.winfo_width():
+    dx = -1
+  elif x <= 0:
+    dx = 1
+  # 画面端にたどり着いたら向きを変える仕組み y軸
+  if y >= canvas.winfo_height():
+    dy = -1
+  elif y <= 0:
+    dy = 1
+
+  # move関数繰り返し
+  root.after(20, move)
 
 
   
