eb8bce38536c3e8e5199171d6794ed4481618720
y8r8gpopd-ux <y8r8g.popd@gmail.com>
2026-06-08 15:31:10 +0900

    複数円をリスト・辞書化で描写

--- grafic.py
+++ grafic.py
@@ -1,40 +1,37 @@
 import tkinter as tk
 
-# ひとつ前の円をフラグで管理
-circle = None
+# 円を辞書で管理
+circles = [
+  {"x": 400, "y": 300, "one_before": None, "dx": 1, "dy": 1, "color": "red"},
+  {"x": 200, "y": 100, "one_before": None, "dx": -1, "dy": 1, "color": "green"},
+  {"x": 100, "y": 200, "one_before": None, "dx": 1, "dy": -1, "color": "blue"}
+]
 
-x = 400
-y = 300
-
-dx = 1
-dy = 1
-
-# クリックした場所に円の作成
 def move():
   # 関数外から読み込み
-  global circle, x, y, dx, dy
+  global circles
 
-  # 描画した円が残っていたら削除
-  if circle != None:
-    canvas.delete(circle)
-  
-  # 座標を移動させるdx,dy
-  x += dx
-  y += dy
+  # 各円の描画
+  for circle in circles:
+    if circle["one_before"] != None:
+      canvas.delete(circle["one_before"])
 
-  # 円の描画
-  circle = canvas.create_oval(x - 20, y - 20, x + 20, y + 20, fill = "red", width = 0)
+    circle["x"] = circle["x"] + circle["dx"]
+    circle["y"] = circle["y"] + circle["dy"]
 
-  # 画面端にたどり着いたら向きを変える仕組み x軸
-  if x >= canvas.winfo_width():
-    dx = -1
-  elif x <= 0:
-    dx = 1
-  # 画面端にたどり着いたら向きを変える仕組み y軸
-  if y >= canvas.winfo_height():
-    dy = -1
-  elif y <= 0:
-    dy = 1
+    circle["one_before"]= canvas.create_oval(circle["x"] - 20, circle["y"] - 20, circle["x"] + 20, circle["y"] + 20,
+    fill = circle["color"], width = 0)
+ 
+    # 画面端にたどり着いたら向きを変える仕組み x軸
+    if circle["x"] >= canvas.winfo_width():
+      circle["dx"] = -1
+    elif circle["x"] <= 0:
+      circle["dx"] = 1
+    # 画面端にたどり着いたら向きを変える仕組み y軸
+    if circle["y"] >= canvas.winfo_height():
+      circle["dy"] = -1
+    elif circle["y"] <= 0:
+      circle["dy"] = 1
 
   # move関数繰り返し
   root.after(20, move)
