83f2496eb9fac00170403b0189c6cde008917af4
y8r8gpopd-ux <y8r8g.popd@gmail.com>
2026-06-08 17:49:21 +0900

    オーバーライドの学習

--- grafic.py
+++ grafic.py
@@ -20,8 +20,8 @@ class Circle():
     self.x = self.x + self.dx
     self.y = self.y + self.dy
 
-    self.one_before = canvas.create_oval(self.x - 20, self.y - 20, self.x + 20, self.y + 20,
-    fill = self.color, width = 0)
+    # 円の描画
+    self.draw(canvas)
  
     # 画面端にたどり着いたら向きを変える仕組み x軸
     if self.x >= canvas.winfo_width():
@@ -34,20 +34,39 @@ class Circle():
     elif self.y <= 0:
       self.dy = 1
 
-# 円のインスタンスをリスト管理
-circles = [
+  def draw(self, canvas):
+    self.one_before = canvas.create_oval(self.x - 20, self.y - 20, self.x + 20, self.y + 20,
+    fill = self.color, width = 0)
+
+
+# オーバーライドで四角と三角を描画できるように
+class Rectangle(Circle):
+  def draw(self, canvas):
+    self.one_before = canvas.create_rectangle(self.x - 20, self.y - 20, self.x + 20, self.y + 20,
+    fill = self.color, width = 0)
+
+class Triangle(Circle):
+  def draw(self, canvas):
+    self.one_before = canvas.create_polygon(self.x, self.y - 20, self.x + 20, self.y + 20, self.x - 20, self.y + 20,
+    fill = self.color, outline = "", width = 0)
+
+
+# 図形のインスタンスをリスト管理
+shapes = [
   Circle(400, 300, 1, 1, "red"),
   Circle(200, 100, -1, 1, "blue"),
   Circle(300, 200, 1, -1, "green"),
   Circle(100, 100, -1, -1, "yellow"),
   Circle(150, 150, 1, -1, "orange"),
-  Circle(400, 400, -1, 1, "#C77EB5")
+  Circle(400, 400, -1, 1, "#C77EB5"),
+  Rectangle(100, 200, 1, 1, "#7795BD"),
+  Triangle(400, 0, 1, -1, "#F181B0")
 ]
 
 # ループさせる関数
 def loop():
-  for circle in circles:
-    circle.move(canvas)
+  for shape in shapes:
+    shape.move(canvas)
 
   root.after(20, loop)
 
