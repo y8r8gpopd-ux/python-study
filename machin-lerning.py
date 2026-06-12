d7d5945c21f03fdad931318913035a0441903d44
y8r8gpopd-ux <y8r8g.popd@gmail.com>
2026-06-12 16:40:22 +0900

    画像の物体認識練習

--- machin-lerning.py
+++ machin-lerning.py
@@ -2,6 +2,11 @@ import tkinter as tk
 import tkinter.messagebox as tkm
 from tkinter import filedialog
 from PIL import Image, ImageTk
+from ultralytics import YOLO
+import cv2
+
+# YOLOの準備
+model = YOLO("yolov8n.pt")
 
 # 前回の画像
 before_img = None
@@ -19,8 +24,18 @@ def ButtonClick():
 
     # 画像読み込み
     img = Image.open(file_name)
+
+    # YOLOで物体検出
+    result = model.predict(source = img)
+    # 結果の画像データを抽出  
+    result_data = result[0].plot()
+    # BGRからRGBに変換  
+    result_rgb = cv2.cvtColor(result_data, cv2.COLOR_BGR2RGB)
+    # Imageデータ化 
+    result_img = Image.fromarray(result_rgb)
+
     # PhotoImage形式に変換
-    pimg = ImageTk.PhotoImage(img)
+    pimg = ImageTk.PhotoImage(image = result_img)
     # キャンバスに表示
     before_img = canvas.create_image(0, 0, anchor = tk.NW, image = pimg)
 
